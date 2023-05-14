2c2b1ab8a096729722cd2bd81f1811383a4a5e34
MarceloRH <58949196+MarceloRH@users.noreply.github.com>
2023-05-14 19:51:16 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema")
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema =document.querySelector(".imagem-botao")
+
+botaoAlterarTema.addEventListener("click", () => {
+
+    
+const modoEscuroEstaAtivo = body.classList.contains("modo-escuro")
+
+body.classList.toggle("modo-escuro");
+
+if(modoEscuroEstaAtivo){
+    
+    body.classList.remove("modo-escuro");
+
+    imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/sun.png");
+    
+} else {
+    body.classList.add("modo-escuro");
+
+    imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/moon.png");
+}
+
+
+
+});
+
+
